64dc874a2098d13d6b97cfc39ccc2a1588e7e6c5
ignazio sammarco <netsam@libero.it>
2021-12-31 15:37:49 +0100

    Error "super" importing Component from react

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,4 +1,4 @@
-import { Component } from 'react/cjs/react.development';
+import { Component } from 'react';
 import { w3cwebsocket as W3CWebSocket } from "websocket";
 import { useRef, useEffect  } from 'react';
 import './Chat.css';
